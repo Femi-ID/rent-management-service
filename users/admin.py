dc5dfb35077415a0a8c51b6077be31c245c64e8c
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2024-06-30 01:38:36 +0100

    feat: register users model in the admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'first_name', 'last_name', 'username', 'is_active', 'email', 'phone_number', 'user_type', 'date_created')
+    list_filter = ('first_name', 'last_name', 'username', 'user_type', 'is_active', 'is_superuser')
+    search_fields = ('username', 'email', 'user_type', )
