834b76db5e1c5effee4c06d0973ba5328be45cee
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2024-08-26 16:57:58 +0100

    chore: update requirements.txt

--- Rent_Management/settings.py
+++ Rent_Management/settings.py
@@ -26,15 +26,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
-# DEBUG = True
+# DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'corsheaders',
+    'rest_framework_swagger',   # Swagger
+    'drf_yasg',    # Another Swagger generator
 ]
 
 AUTH_USER_MODEL = 'users.User'
@@ -110,8 +112,8 @@ DATABASES = {
 }
 database_url = os.environ.get('DATABASE_URL')
 # THIS DATABASE IS FOR DEVELOPMENT ONLY, NOT TESTING
-DATABASES['default'] = dj_database_url.parse('postgresql://rent-db_owner:seyAmqkU4iB3@ep-curly-hall-a2jgryqx.eu-central-1.aws.neon.tech/rent-db?sslmode=require')
-
+# DATABASES['default'] = dj_database_url.parse('postgresql://rent-db_owner:seyAmqkU4iB3@ep-curly-hall-a2jgryqx.eu-central-1.aws.neon.tech/rent-db?sslmode=require')
+DATABASES['default'] = dj_database_url.parse(database_url)
 # THE DATABASE YOU ARE TO WORK WITH WHEN TESTING:
 # DATABASES = {
 #     'default': {
@@ -218,25 +220,25 @@ SITE_NAME = 'RENT PADI'
 DOMAIN = 'localhost:8000' # (change to frontend localhost) the ACTIVATION_URL will be appended to this domain
 
 # Email config
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_PORT = os.environ.get('EMAIL_PORT')
+# EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+# DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
-# EMAIL_BACKEND = config('EMAIL_BACKEND')
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_PORT = config('EMAIL_PORT')
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-# # EMAIL_USE_SSL = config('EMAIL_USE_SSL')
-# DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_USE_SSL = config('EMAIL_USE_SSL')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
 
-PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
-PAYSTACK_PUBLIC_KEY = os.environ.get("PAYSTACK_PUBLIC_KEY")
+# PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
+# PAYSTACK_PUBLIC_KEY = os.environ.get("PAYSTACK_PUBLIC_KEY")
 
-# PAYSTACK_SECRET_KEY = config('PAYSTACK_SECRET_KEY')
-# PAYSTACK_PUBLIC_KEY = config("PAYSTACK_PUBLIC_KEY")
+PAYSTACK_SECRET_KEY = config('PAYSTACK_SECRET_KEY')
+PAYSTACK_PUBLIC_KEY = config("PAYSTACK_PUBLIC_KEY")
--- Rent_Management/urls.py
+++ Rent_Management/urls.py
@@ -17,10 +17,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+# swagger imports
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="RENTPADDI API",
+        default_version='v1',
+        description="API documentation for Rent management service.",
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('accounts/', include('accounts.urls')),
     path('payments/', include('payments.urls')),
+
+    # swagger route for documentation
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
