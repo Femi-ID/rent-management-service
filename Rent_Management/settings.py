c65a004006d1d7adc53d0bcbc84d3af1fdc311d8
zee467 <wura.mahmud@gmail.com>
2024-08-28 19:59:25 +0100

    Settings update

--- Rent_Management/settings.py
+++ Rent_Management/settings.py
@@ -15,8 +15,8 @@ from datetime import timedelta
 import dj_database_url
 from decouple import config
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'accounts', 
     'payments',
     'wallets',
-    'core', 
+    'core',
+    'tickets', 
 
     # added dependencies
     'rest_framework',
