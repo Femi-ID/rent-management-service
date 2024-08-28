ddaabcb0446b5e321b02f297cd2a0f65b4955601
zee467 <wura.mahmud@gmail.com>
2024-08-28 20:00:32 +0100

    Added tickets and house path

--- Rent_Management/urls.py
+++ Rent_Management/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('accounts/', include('accounts.urls')),
     path('payments/', include('payments.urls')),
+    path('tickets/', include('tickets.urls')),
+    path('house/', include('core.urls')),
 
     # swagger route for documentation
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
