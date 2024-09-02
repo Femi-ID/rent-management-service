9c69c5ff67892caf2447c5a5b27d8ee13774cdf0
*Ajayi <*emmanueltemitayo056@gmail.com*>
2024-09-02 16:28:45 +0100

    created functions to send mails when called in other applications

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -1,14 +1,15 @@
 from datetime import timedelta, timezone
 from celery import shared_task
 from notifications.utils import send_notification
-from core.models import LeaseAgreement
+from core.models import LeaseAgreement, HouseUnit
+from django.core.mail import send_mail
+from users.models import User
 
 @shared_task
 def check_rent_due_date(self):
     today_date = timezone.now().date() # get the current date
     
     # Check for rent due in 30 days
-    # assuming the lease agreement model has a field called due_date
     rent_due_soon = LeaseAgreement.objects.filter(rent_due_date = today_date + timedelta(days=30)) # get all lease agreements with rent due in 30 days
 
     #send email to the tenant
@@ -58,3 +59,51 @@ def check_rent_due_date(self):
 
 
 
+@shared_task
+def send_ticket_notification_to_tenant(tenant_id, unit_id):
+    print('entered send_ticket_notification_to_tenant')
+    # Retrieve tenant and unit based on their IDs
+    tenant = User.objects.get(id=tenant_id)
+    unit = HouseUnit.objects.get(id=unit_id)
+    
+
+    # Send email to the tenant
+    print('Email sent successfully 1')
+    send_mail(
+        f'Ticket {unit.pk} Status Updated',
+        f'The status of your ticket "{unit.subject}" has been updated to "open".',
+        'process.env.EMAIL_HOST_USER', #change to your email
+        [tenant.email],
+    )
+    
+
+@shared_task
+# send mail to landlord when a tenants makes a ticket
+def send_ticket_notification_to_landlord(user_id, unit_id):
+
+    # Retrieve user and unit based on their IDs
+    user = User.objects.get(id=user_id)
+    house_unit = HouseUnit.objects.get(id=unit_id)
+    send_mail(
+        'New Ticket Notification',
+        f'Your tenant: {user.email} has made a ticket for {house_unit.unit_number}.',
+        'process.env.EMAIL_HOST_USER', #change to your email host user
+        [house_unit.house.owner.email],   
+        fail_silently=False,
+    )
+
+
+@shared_task
+def send_email_upon_payment(user_id, house_unit_id):
+    # Retrieve user and house_unit based on their IDs
+    user = User.objects.get(id=user_id)
+    house_unit = HouseUnit.objects.get(id=house_unit_id)
+
+    # Send email to the landlord
+    send_mail(
+        'Rent Payment Notification',
+        f'Your tenant: {user.email} has paid their rent for {house_unit.unit_number}.',
+        'process.env.EMAIL_HOST_USER', #change to your email host user
+        [house_unit.house.owner.email],   
+        fail_silently=False,
+    )
--- notifications/utils.py
+++ notifications/utils.py
@@ -9,3 +9,36 @@ def send_notification(user, subject, message):
         fail_silently=False,
     )
     print('Email sent successfully')
+
+
+# send mail upon payment by tenant to landlord
+# def send_email_upon_payment(user, house_unit):
+#     send_mail(
+#         'Rent Payment Notification',
+#         f'Your tenant: {user.email} has paid their rent for {house_unit.unit_number}.',
+#         'process.env.EMAIL_HOST_USER', #change to your email host user
+#         [house_unit.house.owner.email],   
+#         fail_silently=False,
+#     ) 
+
+
+# # send mail to landlord when a tenants makes a ticket
+# def send_ticket_notification_to_landlord(user, house_unit):
+#     send_mail(
+#         'New Ticket Notification',
+#         f'Your tenant: {user.email} has made a ticket for {house_unit.unit_number}.',
+#         [user.email], #change to your email host user
+#         [house_unit.house.owner.email],   
+#         fail_silently=False,
+#     )
+
+
+# # send an email to the tenant when a ticket has been resolved
+# def send_ticket_notification_to_tenant(user, house_unit):
+#     send_mail(
+#         'New Ticket Notification',
+#         f'Your Lanlord: {house_unit.house.owner.email} has resolved the ticket for {house_unit.unit_number}.',
+#         'process.env.EMAIL_HOST_USER', #change to your email host user
+#         [user.email],   
+#         fail_silently=False,
+#     )
