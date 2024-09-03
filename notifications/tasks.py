5c680afc6d3b164845305adf9d4ed0204e080e83
*Ajayi <*emmanueltemitayo056@gmail.com*>
2024-09-03 16:42:48 +0100

    added the filter by payment status when querying the subscription model

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from notifications.utils import send_notification
 from core.models import HouseUnit
 from django.core.mail import send_mail
+from payments.enums import PaymentStatus
 from users.models import User
 from payments.models import Subscription, Payment
 
@@ -11,100 +12,47 @@ def check_rent_due_date(self):
     today_date = timezone.now().date() # get the current date
     
     # Check for rent due in 30 days
-    rent_due_soon = Subscription.objects.filter(next_payment_date = today_date + timedelta(days=30)) # TODO: Check to include the status of the subscription to the filter
+    subscriptions = Subscription.objects.filter(next_payment_date = today_date + timedelta(days=30), status__in=[ PaymentStatus.PENDING, PaymentStatus.FAILED]) 
    
     #send email to the tenant
-    for lease in rent_due_soon:
+    for subscription in subscriptions:
         send_notification(
-        lease.user, 
-        "Your rent is due soon",
-        f"Dear {lease.user.full_name}, your rent for the lease {lease.id} is due on {lease.rent_due_date}. Please ensure payment is made in due time."
+        "Upcoming Rent Due Reminder"
+        f"Dear {subscription.customer.email},\n\nThis is a reminder that your rent for your unit is due soon on {subscription.next_payment_date}.\nPlease ensure timely payment to avoid late fees.\n\nThank you!"
     )
 
 
 
     # check for rent due today
-    rent_due = Subscription.objects.filter(rent_due_date = today_date) # TODO: Check to include the status of the subscription to the filter
+    subscriptions = Subscription.objects.filter(next_payment_date = today_date, status__in=[ PaymentStatus.PENDING, PaymentStatus.FAILED])
 
     #send email to the tenant
-    for lease in rent_due:
+    for subscription in subscriptions:
         send_notification(
-        lease.user, 
-        "Your rent is due today",
-        f"Dear {lease.user.full_name}, your rent for the lease {lease.id} is due today. Please ensure payment is made in due time."
+        "Rent Due Reminder"
+        f"Dear {subscription.customer.email},\n\nThis is a reminder that your rent for your unit is due today.\nPlease ensure timely payment to avoid late fees.\n\nThank you!"
     )
-        
 
     # check for rent due in 1 day
-    rent_due_tomorrow = Subscription.objects.filter(rent_due_date = today_date + timedelta(days=1)) # TODO: Check to include the status of the subscription to the filter
+    subscriptions = Subscription.objects.filter(next_payment_date = today_date + timedelta(days=1), status__in=[ PaymentStatus.PENDING, PaymentStatus.FAILED])
 
     #send email to the tenant
-    for lease in rent_due_tomorrow:
+    for subscription in subscriptions:
         send_notification(
-        lease.user, 
-        "Your rent is due tomorrow",
-        f"Dear {lease.user.full_name}, your rent for the lease {lease.id} is due tomorrow. Please ensure payment is made in due time."
+        "Rent Due Reminder"
+        f"Dear {subscription.customer.email},\n\nThis is a reminder that your rent for your unit is due tomorrow.\nPlease ensure timely payment to avoid late fees.\n\nThank you!"
     )
         
     
     # check for rent overdue
-    rent_overdue = Subscription.objects.filter(rent_due_date__lt = today_date) # TODO: Check to include the status of the subscription to the filter
+    subscriptions = Subscription.objects.filter(next_payment_date__lt = today_date, status__in=[ PaymentStatus.PENDING, PaymentStatus.FAILED])
 
     #send email to the tenant
-    for lease in rent_overdue:
+    for subscription in subscriptions:
         send_notification(
-        lease.user, 
-        "Your rent is overdue",
-        f"Dear {lease.user.full_name}, your rent for the lease {lease.id} was due on {lease.rent_due_date}. Please ensure payment is made in due time to avoid penalties."
+        "Rent Overdue Reminder"
+        f"Dear {subscription.customer.email},\n\nThis is a reminder that your rent for your unit is overdue.\nPlease ensure timely payment to avoid late fees.\n\nThank you!"
     )
 
 
 
-@shared_task
-def send_ticket_notification_to_tenant(tenant_id, unit_id):
-    print('entered send_ticket_notification_to_tenant')
-    # Retrieve tenant and unit based on their IDs
-    tenant = User.objects.get(id=tenant_id)
-    unit = HouseUnit.objects.get(id=unit_id)
-    
-
-    # Send email to the tenant
-    print('Email sent successfully 1')
-    send_mail(
-        f'Ticket {unit.pk} Status Updated',
-        f'The status of your ticket "{unit.subject}" has been updated to "open".',
-        'process.env.EMAIL_HOST_USER', #change to your email
-        [tenant.email],
-    )
-    
-
-@shared_task
-# send mail to landlord when a tenants makes a ticket
-def send_ticket_notification_to_landlord(user_id, unit_id):
-
-    # Retrieve user and unit based on their IDs
-    user = User.objects.get(id=user_id)
-    house_unit = HouseUnit.objects.get(id=unit_id)
-    send_mail(
-        'New Ticket Notification',
-        f'Your tenant: {user.email} has made a ticket for {house_unit.unit_number}.',
-        'process.env.EMAIL_HOST_USER', # This is the sender email
-        [house_unit.house.owner.email],   # This is the receiver email
-        fail_silently=False,
-    )
-
-
-@shared_task
-def send_email_upon_payment(user_id, house_unit_id):
-    # Retrieve user and house_unit based on their IDs
-    user = User.objects.get(id=user_id)
-    house_unit = HouseUnit.objects.get(id=house_unit_id)
-
-    # Send email to the landlord
-    send_mail(
-        'Rent Payment Notification',
-        f'Your tenant: {user.email} has paid their rent for {house_unit.unit_number}.',
-        'process.env.EMAIL_HOST_USER', # This is the sender email
-        [house_unit.house.owner.email], # This is the receiver email
-        fail_silently=False,
-    )
