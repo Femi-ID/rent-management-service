d7a8fb1f4f213e43e788df19ceeb168a6380ecbe
*Ajayi <*emmanueltemitayo056@gmail.com*>
2024-10-28 20:38:39 +0100

    corrected error

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("create-ticket/<int:pk>/", views.CreateTickets.as_view(), name="create_ticket"),
     path("", views.TicketsListView.as_view(), name="ticket_list"),
     path("<int:ticket_id>/", views.TicketDetailsView.as_view(), name="ticket_detail"),
-    path("<int:ticket_id>/", views.TicketUpdateView.as_view(), name="update_ticket"),
-    path("<int:ticket_id>/", views.DeleteTicketView.as_view(), name="delete_ticket"),
+    path("update/<int:ticket_id>/", views.TicketUpdateView.as_view(), name="update_ticket"),
+    path("delete/<int:ticket_id>/", views.DeleteTicketView.as_view(), name="delete_ticket"),
 ]
