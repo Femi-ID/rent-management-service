672ce8a5e7b0d1b152ef8be87a1c1a984cb606e8
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2024-10-29 19:42:01 +0100

    Fix: resolve import error in core/views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-import stat
-from tkinter import ON, W
+# import stat
+# from tkinter import ON, W
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
